0b07abf4c458d070a54f4622d401ffe3a9dd9de1
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-28 13:57:25 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+find /usr -name gamecontrollerdb.txt
+find /etc -name gamecontrollerdb.txt
+exit 1
+
 ARCH=$(uname -m)
 VERSION=$(pacman -Q aaaaxy | awk '{print $2; exit}')
 export ARCH VERSION
